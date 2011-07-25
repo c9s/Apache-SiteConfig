c2d654f7df72b42ad58a9cabfccb10f373240126
c9s <cornelius.howl@gmail.com>
2011-07-25 16:47:41 +0800

    Bump version

--- lib/Apache/SiteConfig.pm
+++ lib/Apache/SiteConfig.pm
@@ -1,7 +1,7 @@
 package Apache::SiteConfig;
 use strict;
 use warnings;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 use Apache::SiteConfig::Statement;
 use Apache::SiteConfig::Section;
 use Apache::SiteConfig::Directive;
